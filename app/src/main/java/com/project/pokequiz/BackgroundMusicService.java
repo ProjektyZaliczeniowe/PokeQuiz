ff0747766e4b3cf7adc184032196953b256e0bca
krzysiekp25 <krzysiek251p@gmail.com>
2019-11-23 21:37:04 +0100

    Added background music in service with on/off button. Boolean on/off added in SharedPreferences

---
+++ app/src/main/java/com/project/pokequiz/BackgroundMusicService.java
@@ -0,0 +1,34 @@
+package com.project.pokequiz;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class BackgroundMusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+//todo boundservice przy zmianie aktywnosci
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        mediaPlayer.stop();
+    }
+}
--- app/src/main/java/com/project/pokequiz/MainActivity.java
+++ app/src/main/java/com/project/pokequiz/MainActivity.java
@@ -1,13 +1,49 @@
 package com.project.pokequiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean musicOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageButton musicButton = findViewById(R.id.imageButton);
+        configureBackgorundMusic(musicButton);
+    }
+
+    private void configureBackgorundMusic(final ImageButton musicButton) {
+        final SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("sharedPreferences", 0);
+        musicOn = sharedPreferences.getBoolean("musicOn", true);
+        if (musicOn) {
+            startService(new Intent(this, BackgroundMusicService.class));
+            musicButton.setBackgroundResource(R.drawable.music_on);
+        } else {
+            musicButton.setBackgroundResource(R.drawable.music_off);
+        }
+
+        musicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(musicOn) {
+                    musicOn = false;
+                    sharedPreferences.edit().putBoolean("musicOn", false).apply();
+                    musicButton.setBackgroundResource(R.drawable.music_off);
+                    stopService(new Intent(v.getContext(), BackgroundMusicService.class));
+                } else {
+                    musicOn = true;
+                    sharedPreferences.edit().putBoolean("musicOn", true).apply();
+                    musicButton.setBackgroundResource(R.drawable.music_on);
+                    startService(new Intent(v.getContext(), BackgroundMusicService.class));
+                }
+            }
+        });
     }
 }
