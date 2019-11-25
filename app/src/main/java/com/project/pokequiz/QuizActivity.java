3e96fda2fc6011c310a8a07373266faa3a47d2b6
nankiewiczka <karolina.nankiewicz@gmail.com>
2019-11-26 00:05:32 +0100

    Add fragment for using in questions

--- app/src/main/java/com/project/pokequiz/QuizActivity.java
+++ app/src/main/java/com/project/pokequiz/QuizActivity.java
@@ -1,7 +1,11 @@
 package com.project.pokequiz;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -12,5 +16,58 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Button btnA = this.findViewById(R.id.answerA);
+        Button btnB = this.findViewById(R.id.answerB);
+        Button btnC = this.findViewById(R.id.answerC);
+        Button btnD = this.findViewById(R.id.answerD);
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = QuestionFragment.newInstance("A", "B");
+                Bundle bundle = new Bundle();
+                bundle.putString("question", "Pytanie A");
+                fragment.setArguments(bundle);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.question_frag, fragment).commit();
+            }
+        });
+
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = QuestionFragment.newInstance("A", "B"); // TODO constructor for change,try only if works but not
+                Bundle bundle = new Bundle();
+                bundle.putString("question", "Pytanie B");
+                fragment.setArguments(bundle);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.question_frag, fragment).commit();
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new QuestionFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("question", "Pytanie C");
+                fragment.setArguments(bundle);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.question_frag, fragment).commit();
+            }
+        });
+
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new QuestionFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("question", "Pytanie D");
+                fragment.setArguments(bundle);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.question_frag, fragment).commit();
+            }
+        });
     }
 }
