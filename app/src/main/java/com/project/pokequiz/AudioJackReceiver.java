6b0b6a8359971606873b85f90b21720d5880cf72
nankiewiczka <karolina.nankiewicz@gmail.com>
2019-12-01 17:00:39 +0100

    Change volume to 20% of max volume when plug headset

--- app/src/main/java/com/project/pokequiz/AudioJackReceiver.java
+++ app/src/main/java/com/project/pokequiz/AudioJackReceiver.java
@@ -15,16 +15,20 @@ public class AudioJackReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(AudioManager.ACTION_HEADSET_PLUG)) {
             int state = intent.getIntExtra("state", -1);
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
             switch (state) {
                 case 0:
-                    if(plugState != -1)
+                    if(plugState != 0 && plugState != -1)
                         Toast.makeText(context, R.string.headphones_off, Toast.LENGTH_LONG).show();
                     plugState = -1;
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
                     break;
                 case 1:
-                    if(plugState != 1 )
+                    if(plugState != 0 && plugState != 1)
                         Toast.makeText(context, R.string.headphones_on, Toast.LENGTH_LONG).show();
                     plugState = 1;
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,  (int)(maxVolume*0.2), 0);
                     break;
                 default:
                     break;
